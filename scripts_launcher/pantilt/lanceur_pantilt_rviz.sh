fa1fe88c53ca94d4f50d73e4ea6358cbdf5b28c9
lalequie <laurent.lequievre@univ-bpclermont.fr>
2017-09-06 17:41:53 +0200

    Set new directory for workspace

--- scripts_launcher/pantilt/lanceur_pantilt_rviz.sh
+++ scripts_launcher/pantilt/lanceur_pantilt_rviz.sh
@@ -5,10 +5,10 @@
 # UMR 6602 - Institut Pascal
 
 
-cd /home/pantilt/projects/ros_sigma_platform_fri_ws
+cd /home/pantilt/git_project/platform-sigma
 source devel/setup.bash
 export ROS_MASTER_URI=http://ifma-kuka-test:11311
-roslaunch single_lwr_robot right_arm_rviz.launch
+roslaunch platform_rviz platform_rviz.launch
 
 
 
