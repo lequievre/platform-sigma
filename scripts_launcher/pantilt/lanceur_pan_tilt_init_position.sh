4132015e7b1c123384ceef23a12182145390dae4
lalequie <laurent.lequievre@univ-bpclermont.fr>
2017-09-07 15:18:25 +0200

    Change name of workspace directory

--- scripts_launcher/pantilt/lanceur_pan_tilt_init_position.sh
+++ scripts_launcher/pantilt/lanceur_pan_tilt_init_position.sh
@@ -4,6 +4,6 @@
 # laurent.lequievre@univ-bpclermont.fr
 # UMR 6602 - Institut Pascal
 
-source /home/pantilt/projects/catkin_sigma_ws/devel/setup.bash
+source /home/pantilt/git_project/platform-sigma/devel/setup.bash
  rostopic pub -1 /pantilt/pan_tilt_position_controller/command std_msgs/Float64MultiArray "data: [1.5,0.0,0.0,0.0]"
 
--- scripts_launcher/pantilt/lanceur_pan_tilt_without_camera.sh
+++ scripts_launcher/pantilt/lanceur_pan_tilt_without_camera.sh
@@ -4,6 +4,6 @@
 # laurent.lequievre@univ-bpclermont.fr
 # UMR 6602 - Institut Pascal
 
-source /home/pantilt/projects/catkin_sigma_ws/devel/setup.bash
+source /home/pantilt/git_project/platform-sigma/devel/setup.bash
 roslaunch pan_tilt_real pan_tilt_real.launch use_camera:=false view_camera:=false
 
