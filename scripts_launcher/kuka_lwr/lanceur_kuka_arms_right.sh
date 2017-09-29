68a71564da1625186dbc488e855ec97d677dc7a6
Laurent LEQUIEVRE <laurent.lequievre@univ-bpclermont.fr>
2017-09-29 11:49:20 +0200

    Add Master ros uri path

--- scripts_launcher/kuka_lwr/lanceur_kuka_arms_right.sh
+++ scripts_launcher/kuka_lwr/lanceur_kuka_arms_right.sh
@@ -4,6 +4,7 @@
 # laurent.lequievre@uca.fr
 # UMR 6602 - Institut Pascal
 
+export ROS_MASTER_URI=http://ifma-kuka-test:11311
 cd /home/ifma/git_project/platform-sigma
 source devel/setup.bash
 roslaunch double_lwr_robot double_arms.launch use_left_arm:=false
