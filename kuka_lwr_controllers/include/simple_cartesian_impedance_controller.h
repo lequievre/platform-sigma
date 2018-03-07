2064b60ef81eeebc642eab22fce0194266f33913
Laurent LEQUIEVRE <laurent.lequievre@univ-bpclermont.fr>
2018-03-07 18:05:22 +0100

    Adding topic to set cartesian stiffness and damping command

--- kuka_lwr_controllers/include/simple_cartesian_impedance_controller.h
+++ kuka_lwr_controllers/include/simple_cartesian_impedance_controller.h
@@ -14,6 +14,11 @@
 // hardware_interface 
 #include <kuka_lwr_hw/lwr_kuka_cartesian_interface.h> // contains definition of KUKACartesianInterface
 
+// msgs Float64MultiArray
+#include <std_msgs/Float64MultiArray.h>
+
+#define TRACE_CARTESIAN_IMPENDANCE_CONTROLLER_ACTIVATED 1
+
 namespace kuka_lwr_controllers
 {
 	class SimpleCartesianImpedanceController: 
@@ -27,8 +32,10 @@ namespace kuka_lwr_controllers
 			void starting(const ros::Time& time);  // Start the controller
 			void stopping(const ros::Time& time);  // Stop the controller
 			void update(const ros::Time& time, const ros::Duration& period);  // Update the controller
-			//void setStiffnessDamping(const kuka_lwr_controllers::StiffnessDamping::ConstPtr & msg);
+			void setCartesianStiffness(const std_msgs::Float64MultiArrayConstPtr& msg);
+			void setCartesianDamping(const std_msgs::Float64MultiArrayConstPtr& msg);
 			//void commandCB(const std_msgs::Float64MultiArrayConstPtr& msg); // function associate to a subscribe command topic
+			
 			
 		private:
 			std::string robot_namespace_;
@@ -36,6 +43,11 @@ namespace kuka_lwr_controllers
 			hardware_interface::KUKACartesianDampingHandle kuka_cart_damp_handle_;
 			
 			int trace_count_ = 0;
+			
+			ros::Subscriber sub_cart_stiffness_command_, sub_cart_damping_command_;
+			
+			static constexpr int NUMBER_OF_CART_DOFS = 6;
+			static constexpr int NUMBER_OF_FRAME_ELEMENTS = 12;
 			
 			/*KDL::JntArray  stiff_, damp_, q_des_; // stiffness and damping values got from topic
 			ros::Subscriber sub_stiffness_damping_, sub_command_; // subscribers of stiffness and damping and command position
--- kuka_lwr_controllers/src/simple_cartesian_impedance_controller.cpp
+++ kuka_lwr_controllers/src/simple_cartesian_impedance_controller.cpp
@@ -18,14 +18,17 @@ namespace kuka_lwr_controllers
      bool SimpleCartesianImpedanceController::init(hardware_interface::KUKACartesianInterface *robot, ros::NodeHandle &n)
      {
 		robot_namespace_ = n.getNamespace();
-		
+		// robot_namespace_ contains the namespace concatenate with the name of this controller
+		// example : /kuka_lwr_left/kuka_simple_cartesian_impedance_controller
+		// Only need to get the namespace --------------------------------------------
 		int pos_found = robot_namespace_.find("/",1);
 		std::string robot_namespace_only = robot_namespace_.substr(1,pos_found-1);
+		// ----------------------------------------------------------------------------
 		 
-		//#if TRACE_ACTIVATED
+		#if TRACE_CARTESIAN_IMPENDANCE_CONTROLLER_ACTIVATED
 			ROS_INFO("SimpleCartesianImpedanceController: Start init of robot %s !",robot_namespace_.c_str());
 			ROS_INFO("SimpleCartesianImpedanceController: Start init of robot ns only %s !",robot_namespace_only.c_str());
-		//#endif
+		#endif
 		
 			
         if( !(KinematicChainControllerBase<hardware_interface::KUKACartesianInterface>::init(robot, n)) )
@@ -34,8 +37,12 @@ namespace kuka_lwr_controllers
             return false;
         }
         
+        // Get Cartesian Stiffness and Damping Interfaces Handles
         kuka_cart_stiff_handle_ = robot->getHandle(robot_namespace_only + "_cart_stiffness");
         kuka_cart_damp_handle_ = robot->getHandle(robot_namespace_only + "_cart_damping");
+        
+        sub_cart_stiffness_command_ = n.subscribe("setCartesianStiffness", 1, &SimpleCartesianImpedanceController::setCartesianStiffness, this); 
+        sub_cart_damping_command_ = n.subscribe("setCartesianDamping", 1, &SimpleCartesianImpedanceController::setCartesianDamping, this); 
        
         return true;
         
@@ -44,7 +51,7 @@ namespace kuka_lwr_controllers
 	 void SimpleCartesianImpedanceController::starting(const ros::Time& time)
      {
         
-		#if TRACE_ACTIVATED
+		#if TRACE_CARTESIAN_IMPENDANCE_CONTROLLER_ACTIVATED
 			ROS_INFO("SimpleCartesianImpedanceController: Starting of robot %s !",robot_namespace_.c_str());
 		#endif
 		
@@ -53,7 +60,7 @@ namespace kuka_lwr_controllers
     
      void SimpleCartesianImpedanceController::stopping(const ros::Time& time)
 	 {
-		#if TRACE_ACTIVATED
+		#if TRACE_CARTESIAN_IMPENDANCE_CONTROLLER_ACTIVATED
 			ROS_INFO_NAMED("SimpleCartesianImpedanceController: Stopping of robot %s !",robot_namespace_.c_str());
 		#endif
 		
@@ -61,15 +68,69 @@ namespace kuka_lwr_controllers
 	 
 	 void SimpleCartesianImpedanceController::update(const ros::Time& time, const ros::Duration& period)
      {
-		 trace_count_++;
-		 if (trace_count_%1000)
-		 {
-			ROS_INFO("X stiff = %f, A stiff = %f", kuka_cart_stiff_handle_.getX(), kuka_cart_stiff_handle_.getA());
-			for (int i=0; i<7; i++)
-			{
-				ROS_INFO("Joint Pos(%d) = %f", i, joint_handles_[i].getPosition());
-			}
-		 }
+		 #if TRACE_CARTESIAN_IMPENDANCE_CONTROLLER_ACTIVATED
+			 trace_count_++;
+			 if (trace_count_%10000)
+			 {
+				ROS_INFO("-> stiffness -> X = %f, Y = %f, Z = %f, A = %f, B = %f, C = %f", kuka_cart_stiff_handle_.getX(), kuka_cart_stiff_handle_.getY(), kuka_cart_stiff_handle_.getZ(), kuka_cart_stiff_handle_.getA(), kuka_cart_stiff_handle_.getB(), kuka_cart_stiff_handle_.getC());
+				/*for (int i=0; i<7; i++)
+				{
+					ROS_INFO("Joint Pos(%d) = %f", i, joint_handles_[i].getPosition());
+				}*/
+			 }
+		 #endif
+	 }
+	 
+	 void SimpleCartesianImpedanceController::setCartesianStiffness(const std_msgs::Float64MultiArrayConstPtr& msg)
+	 {
+		#if TRACE_CARTESIAN_IMPENDANCE_CONTROLLER_ACTIVATED
+			ROS_INFO("SimpleCartesianImpedanceController: Start setCartesianStiffness of robot %s!",robot_namespace_.c_str());
+		#endif
+
+		if(msg->data.size()!=NUMBER_OF_CART_DOFS)
+		{ 
+			ROS_ERROR_STREAM("SimpleCartesianImpedanceController: Dimension (of robot " << robot_namespace_.c_str() << ") of command (" << msg->data.size() << ") does not match number of DOF(" << NUMBER_OF_CART_DOFS << ")! Not executing!");
+			return; 
+		}
+		
+		#if TRACE_CARTESIAN_IMPENDANCE_CONTROLLER_ACTIVATED
+			ROS_INFO("Get SimpleCartesianImpedanceController/setCartesianStiffness X=%f, Y=%f, Z=%f, A=%f, B=%f, C=%f",msg->data[0],msg->data[1],msg->data[2],msg->data[3],msg->data[4],msg->data[5]);
+		#endif
+		
+		// 0: X, 1: Y, 2: Z, 3: A, 4: B, 5: C
+		kuka_cart_stiff_handle_.setCommandX(msg->data[0]);
+		kuka_cart_stiff_handle_.setCommandY(msg->data[1]);
+		kuka_cart_stiff_handle_.setCommandZ(msg->data[2]);
+		kuka_cart_stiff_handle_.setCommandA(msg->data[3]);
+		kuka_cart_stiff_handle_.setCommandB(msg->data[4]);
+		kuka_cart_stiff_handle_.setCommandC(msg->data[5]);
+		
+	 }
+	 
+	 void SimpleCartesianImpedanceController::setCartesianDamping(const std_msgs::Float64MultiArrayConstPtr& msg)
+	 {
+		#if TRACE_CARTESIAN_IMPENDANCE_CONTROLLER_ACTIVATED
+			ROS_INFO("SimpleCartesianImpedanceController: Start setCartesianDamping of robot %s!",robot_namespace_.c_str());
+		#endif
+
+		if(msg->data.size()!=NUMBER_OF_CART_DOFS)
+		{ 
+			ROS_ERROR_STREAM("SimpleCartesianImpedanceController: Dimension (of robot " << robot_namespace_.c_str() << ") of command (" << msg->data.size() << ") does not match number of DOF(" << NUMBER_OF_CART_DOFS << ")! Not executing!");
+			return; 
+		}
+		
+		#if TRACE_CARTESIAN_IMPENDANCE_CONTROLLER_ACTIVATED
+			ROS_INFO("Get SimpleCartesianImpedanceController/setCartesianDamping X=%f, Y=%f, Z=%f, A=%f, B=%f, C=%f",msg->data[0],msg->data[1],msg->data[2],msg->data[3],msg->data[4],msg->data[5]);
+		#endif
+		
+		// 0: X, 1: Y, 2: Z, 3: A, 4: B, 5: C
+		kuka_cart_damp_handle_.setCommandX(msg->data[0]);
+		kuka_cart_damp_handle_.setCommandY(msg->data[1]);
+		kuka_cart_damp_handle_.setCommandZ(msg->data[2]);
+		kuka_cart_damp_handle_.setCommandA(msg->data[3]);
+		kuka_cart_damp_handle_.setCommandB(msg->data[4]);
+		kuka_cart_damp_handle_.setCommandC(msg->data[5]);
+		
 	 }
 }
 
