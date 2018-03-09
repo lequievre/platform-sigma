dd7ac57128bcf2e7f80b42e1c53312e7006c84bf
Laurent LEQUIEVRE <laurent.lequievre@univ-bpclermont.fr>
2018-03-09 11:21:00 +0100

    Adding cartesian pose to interfaces

--- kuka_lwr_hw/include/kuka_lwr_hw/lwr_kuka_cartesian_interface.h
+++ kuka_lwr_hw/include/kuka_lwr_hw/lwr_kuka_cartesian_interface.h
@@ -14,6 +14,104 @@
 
 namespace hardware_interface
 {
+	
+	/** A KUKA cartesian pose state handle class. */
+	class KukaCartesianPoseStateHandle
+	{
+		public:
+			KukaCartesianPoseStateHandle(): name_(), rxx_(0), rxy_(0), rxz_(0), tx_(0), ryx_(0), ryy_(0), ryz_(0), ty_(0), rzx_(0), rzy_(0), rzz_(0), tz_(0) {}
+			
+			KukaCartesianPoseStateHandle(const std::string& name, const double* rxx, const double* rxy, const double* rxz, const double* tx, const double* ryx, const double* ryy, const double* ryz, const double* ty, const double* rzx, const double* rzy, const double* rzz, const double* tz)
+			: name_(name), rxx_(rxx), rxy_(rxy), rxz_(rxz), ryx_(ryx), ryy_(ryy), ryz_(ryz), rzx_(rzx), rzy_(rzy), rzz_(rzz), tx_(tx), ty_(ty), tz_(tz)
+		    {
+				if (!rxx)
+				{
+				  throw HardwareInterfaceException("Cannot create Kuka Cartesian Pose State handle '" + name + "'. RXX data pointer is null.");
+				}
+				if (!rxy)
+				{
+				  throw HardwareInterfaceException("Cannot create Kuka Cartesian Pose State handle '" + name + "'. RXY data pointer is null.");
+				}
+				if (!rxz)
+				{
+				  throw HardwareInterfaceException("Cannot create Kuka Cartesian Pose State handle '" + name + "'. RXZ data pointer is null.");
+				}
+				
+				if (!ryx)
+				{
+				  throw HardwareInterfaceException("Cannot create Kuka Cartesian Pose State handle '" + name + "'. RYX data pointer is null.");
+				}
+				if (!ryy)
+				{
+				  throw HardwareInterfaceException("Cannot create Kuka Cartesian Pose State handle '" + name + "'. RYY data pointer is null.");
+				}
+				if (!ryz)
+				{
+				  throw HardwareInterfaceException("Cannot create Kuka Cartesian Pose State handle '" + name + "'. RYZ data pointer is null.");
+				}
+				
+				if (!rzx)
+				{
+				  throw HardwareInterfaceException("Cannot create Kuka Cartesian Pose State handle '" + name + "'. RZX data pointer is null.");
+				}
+				if (!rzy)
+				{
+				  throw HardwareInterfaceException("Cannot create Kuka Cartesian Pose State handle '" + name + "'. RZY data pointer is null.");
+				}
+				if (!rzz)
+				{
+				  throw HardwareInterfaceException("Cannot create Kuka Cartesian Pose State handle '" + name + "'. RZZ data pointer is null.");
+				}
+				
+				if (!tx)
+				{
+				  throw HardwareInterfaceException("Cannot create Kuka Cartesian Pose State handle '" + name + "'. TX data pointer is null.");
+				}
+				if (!ty)
+				{
+				  throw HardwareInterfaceException("Cannot create Kuka Cartesian Pose State handle '" + name + "'. TY data pointer is null.");
+				}
+				if (!tz)
+				{
+				  throw HardwareInterfaceException("Cannot create Kuka Cartesian Pose State handle '" + name + "'. TZ data pointer is null.");
+				}
+				
+		    }
+			
+			std::string getName()     const {return name_;}
+			
+			double getRXX()      	  const {assert(rxx_); return *rxx_;}
+			double getRXY()      	  const {assert(rxy_); return *rxy_;}
+			double getRXZ()      	  const {assert(rxz_); return *rxz_;}
+			
+			double getRYX()      	  const {assert(ryx_); return *ryx_;}
+			double getRYY()      	  const {assert(ryy_); return *ryy_;}
+			double getRYZ()      	  const {assert(ryz_); return *ryz_;}
+			
+			double getRZX()      	  const {assert(rzx_); return *rzx_;}
+			double getRZY()      	  const {assert(rzy_); return *rzy_;}
+			double getRZZ()      	  const {assert(rzz_); return *rzz_;}
+			
+			double getTX()      	  const {assert(tx_); return *tx_;}
+			double getTY()      	  const {assert(ty_); return *ty_;}
+			double getTZ()      	  const {assert(tz_); return *tz_;}
+				
+		private:
+		  std::string name_;
+		  const double* rxx_;
+		  const double* rxy_;
+		  const double* rxz_;
+		  const double* tx_;
+		  const double* ryx_;
+		  const double* ryy_;
+		  const double* ryz_;
+		  const double* ty_;
+		  const double* rzx_;
+		  const double* rzy_;
+		  const double* rzz_;
+		  const double* tz_; 
+	};
+	
 	/** A KUKA cartesian stiffness state handle class. */
 	class KukaCartesianStiffnessStateHandle
 	{
@@ -125,6 +223,128 @@ namespace hardware_interface
 		  const double* a_;
 		  const double* b_;
 		  const double* c_;
+	};
+	
+	/** \brief A handle used to read and command a KUKA cartesian pose. */
+	class KUKACartesianPoseHandle : public KukaCartesianPoseStateHandle
+	{
+		public:
+			KUKACartesianPoseHandle() : KukaCartesianPoseStateHandle(), 
+			rxx_cmd_(0), rxy_cmd_(0), rxz_cmd_(0), tx_cmd_(0), 
+			ryx_cmd_(0), ryy_cmd_(0), ryz_cmd_(0), ty_cmd_(0), 
+			rzx_cmd_(0), rzy_cmd_(0), rzz_cmd_(0), tz_cmd_(0) 
+			{}
+			
+			/**
+			* \param js This pose's state handle
+			* \param cmd A pointer to the storage for this joint's output command
+			*/
+			KUKACartesianPoseHandle(const KukaCartesianPoseStateHandle& js, double* rxx_cmd, double* rxy_cmd, double* rxz_cmd, double* tx_cmd, double* ryx_cmd, double* ryy_cmd, double* ryz_cmd, double* ty_cmd, double* rzx_cmd, double* rzy_cmd, double* rzz_cmd, double* tz_cmd)
+			: KukaCartesianPoseStateHandle(js), 
+				rxx_cmd_(rxx_cmd), rxy_cmd_(rxy_cmd), rxz_cmd_(rxz_cmd), tx_cmd_(tx_cmd),
+				ryx_cmd_(ryx_cmd), ryy_cmd_(ryy_cmd), ryz_cmd_(ryz_cmd), ty_cmd_(ty_cmd),
+				rzx_cmd_(rzx_cmd), rzy_cmd_(rzy_cmd), rzz_cmd_(rzz_cmd), tz_cmd_(tz_cmd)
+			  {
+				if (!rxx_cmd_)
+				{
+				  throw HardwareInterfaceException("Cannot create handle '" + js.getName() + "'. Command [KUKA Cartesian Pose] RXX pointer is null.");
+				}
+				if (!rxy_cmd_)
+				{
+				  throw HardwareInterfaceException("Cannot create handle '" + js.getName() + "'. Command [KUKA Cartesian Pose] RXY pointer is null.");
+				}
+				if (!rxz_cmd_)
+				{
+				  throw HardwareInterfaceException("Cannot create handle '" + js.getName() + "'. Command [KUKA Cartesian Pose] RXZ pointer is null.");
+				}
+				if (!tx_cmd_)
+				{
+				  throw HardwareInterfaceException("Cannot create handle '" + js.getName() + "'. Command [KUKA Cartesian Pose] TX pointer is null.");
+				}
+				
+				if (!ryx_cmd_)
+				{
+				  throw HardwareInterfaceException("Cannot create handle '" + js.getName() + "'. Command [KUKA Cartesian Pose] RYX pointer is null.");
+				}
+				if (!ryy_cmd_)
+				{
+				  throw HardwareInterfaceException("Cannot create handle '" + js.getName() + "'. Command [KUKA Cartesian Pose] RYY pointer is null.");
+				}
+				if (!ryz_cmd_)
+				{
+				  throw HardwareInterfaceException("Cannot create handle '" + js.getName() + "'. Command [KUKA Cartesian Pose] RYZ pointer is null.");
+				}
+				if (!ty_cmd_)
+				{
+				  throw HardwareInterfaceException("Cannot create handle '" + js.getName() + "'. Command [KUKA Cartesian Pose] TY pointer is null.");
+				}
+				
+				if (!rzx_cmd_)
+				{
+				  throw HardwareInterfaceException("Cannot create handle '" + js.getName() + "'. Command [KUKA Cartesian Pose] RZX pointer is null.");
+				}
+				if (!rzy_cmd_)
+				{
+				  throw HardwareInterfaceException("Cannot create handle '" + js.getName() + "'. Command [KUKA Cartesian Pose] RZY pointer is null.");
+				}
+				if (!rzz_cmd_)
+				{
+				  throw HardwareInterfaceException("Cannot create handle '" + js.getName() + "'. Command [KUKA Cartesian Pose] RZZ pointer is null.");
+				}
+				if (!tz_cmd_)
+				{
+				  throw HardwareInterfaceException("Cannot create handle '" + js.getName() + "'. Command [KUKA Cartesian Pose] TZ pointer is null.");
+				}
+	
+			  }
+			  
+		  void setCommandRXX(double command)    {assert(rxx_cmd_); *rxx_cmd_ = command;}
+		  void setCommandRXY(double command)    {assert(rxy_cmd_); *rxy_cmd_ = command;}
+		  void setCommandRXZ(double command)    {assert(rxz_cmd_); *rxz_cmd_ = command;}
+		  void setCommandTX(double command)     {assert(tx_cmd_);  *tx_cmd_ = command;}
+		  
+		  void setCommandRYX(double command)    {assert(ryx_cmd_); *ryx_cmd_ = command;}
+		  void setCommandRYY(double command)    {assert(ryy_cmd_); *ryy_cmd_ = command;}
+		  void setCommandRYZ(double command)    {assert(ryz_cmd_); *ryz_cmd_ = command;}
+		  void setCommandTY(double command)     {assert(ty_cmd_);  *ty_cmd_ = command;}
+		  
+		  void setCommandRZX(double command)    {assert(rzx_cmd_); *rzx_cmd_ = command;}
+		  void setCommandRZY(double command)    {assert(rzy_cmd_); *rzy_cmd_ = command;}
+		  void setCommandRZZ(double command)    {assert(rzz_cmd_); *rzz_cmd_ = command;}
+		  void setCommandTZ(double command)     {assert(tz_cmd_);  *tz_cmd_ = command;}
+		  
+		  double getCommandRXX()    const {assert(rxx_cmd_); return *rxx_cmd_;}
+		  double getCommandRXY()    const {assert(rxy_cmd_); return *rxy_cmd_;}
+		  double getCommandRXZ()    const {assert(rxz_cmd_); return *rxz_cmd_;}
+		  double getCommandTX()    const {assert(tx_cmd_); return *tx_cmd_;}
+		  
+		  double getCommandRYX()    const {assert(ryx_cmd_); return *ryx_cmd_;}
+		  double getCommandRYY()    const {assert(ryy_cmd_); return *ryy_cmd_;}
+		  double getCommandRYZ()    const {assert(ryz_cmd_); return *ryz_cmd_;}
+		  double getCommandTY()    const {assert(ty_cmd_); return *ty_cmd_;}
+		  
+		  double getCommandRZX()    const {assert(rzx_cmd_); return *rzx_cmd_;}
+		  double getCommandRZY()    const {assert(rzy_cmd_); return *rzy_cmd_;}
+		  double getCommandRZZ()    const {assert(rzz_cmd_); return *rzz_cmd_;}
+		  double getCommandTZ()    const {assert(tz_cmd_); return *tz_cmd_;}
+		  
+	  private:
+
+		  double* rxx_cmd_;
+		  double* rxy_cmd_;
+		  double* rxz_cmd_;
+		  double* tx_cmd_;
+		  
+		  double* ryx_cmd_;
+		  double* ryy_cmd_;
+		  double* ryz_cmd_;
+		  double* ty_cmd_;
+		  
+		  double* rzx_cmd_;
+		  double* rzy_cmd_;
+		  double* rzz_cmd_;
+		  double* tz_cmd_;
+
 	};
 	
 	
@@ -280,6 +500,16 @@ namespace hardware_interface
 	class KUKACartesianDampingCommandInterface : public HardwareResourceManager<KUKACartesianDampingHandle, ClaimResources> {};
 
 	class KUKACartesianDampingInterface : public KUKACartesianDampingCommandInterface {};
+	
+	
+	class KukaCartesianPoseStateInterface : public HardwareResourceManager<KukaCartesianPoseStateHandle> {};
+
+	class KUKACartesianPoseCommandInterface : public HardwareResourceManager<KUKACartesianPoseHandle, ClaimResources> {};
+
+	class KUKACartesianPoseInterface : public KUKACartesianPoseCommandInterface {};
+	
+	
+	
 	
 	
 	
