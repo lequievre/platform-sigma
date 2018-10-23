ce0ec750032da2dac7e8c4084e2fe3511b0b550d
lequievre <laurent.lequievre@gmail.com>
2018-10-23 16:10:58 +0200

    Modifying namespace topic

--- platform_gazebo/send_specific_position_grasp.sh
+++ platform_gazebo/send_specific_position_grasp.sh
@@ -13,13 +13,13 @@ read -n 1 -p "Press enter to send next position"
 
 rostopic pub -1 /$namespace/kuka_group_command_controller_fri/command std_msgs/Float64MultiArray "data: [1.46,-2.0,0.0,0.0,0.0,0.0,-2.96]"
 
-read -n 1 -p "Press enter to send next position"
+read -n 1 -p "Press enter to send next position to left shadow hand"
 
-rostopic pub -1 /lh/sh_lh_ffj0_position_controller/command std_msgs/Float64 --120
-rostopic pub -1 /lh/sh_lh_mfj0_position_controller/command std_msgs/Float64 -- 95
-rostopic pub -1 /lh/sh_lh_rfj0_position_controller/command std_msgs/Float64 -- 90
-rostopic pub -1 /lh/sh_lh_lfj0_position_controller/command std_msgs/Float64 -- 70
-rostopic pub -1 /lh/sh_lh_wrj1_position_controller/command std_msgs/Float64 -- 19
-rostopic pub -1 /lh/sh_lh_wrj2_position_controller/command std_msgs/Float64 -- -2
+rostopic pub -1 /lsh/sh_lh_ffj0_position_controller/command std_msgs/Float64 --120
+rostopic pub -1 /lsh/sh_lh_mfj0_position_controller/command std_msgs/Float64 -- 95
+rostopic pub -1 /lsh/sh_lh_rfj0_position_controller/command std_msgs/Float64 -- 90
+rostopic pub -1 /lsh/sh_lh_lfj0_position_controller/command std_msgs/Float64 -- 70
+rostopic pub -1 /lsh/sh_lh_wrj1_position_controller/command std_msgs/Float64 -- 19
+rostopic pub -1 /lsh/sh_lh_wrj2_position_controller/command std_msgs/Float64 -- -2
 
 
